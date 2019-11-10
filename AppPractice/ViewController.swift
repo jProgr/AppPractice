938a1a2fedd35c2d2d8225c60234dff9ce92f33d
jProgr <8537691@gmail.com>
2019-11-09 19:54:05 -0600

    Initial Commit

---
+++ AppPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppPractice
+//
+//  Created by Cristóbal on 09/11/19.
+//  Copyright © 2019 jProgr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
