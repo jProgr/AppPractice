8a1fc08b1d646017360e534e00758af9cc46dc37
jProgr <8537691@gmail.com>
2019-11-10 09:48:20 -0600

    Add UI programmatically

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -1,20 +1,20 @@
-//
-//  ViewController.swift
-//  AppPractice
-//
-//  Created by Cristóbal on 09/11/19.
-//  Copyright © 2019 jProgr. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let label = UILabel()
+        label.frame = CGRect(x: 125, y: 125, width: 200, height: 80)
+        label.text = "This is a label"
+        view.addSubview(label)
+
+        let button = UIButton()
+        button.frame = CGRect(x: 125, y: 300, width: 80, height: 80)
+        button.setTitle("Button", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
     }
-
-
 }
-
