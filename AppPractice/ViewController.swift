844236995f8a28b11a62ea6b026635322e595250
jProgr <8537691@gmail.com>
2019-11-13 20:58:30 -0600

    Connect interface objects to code

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -2,6 +2,9 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet var labelResult: UILabel!
+    @IBOutlet var textMessage: UITextField!
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
