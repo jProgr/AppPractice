4ee5fe7539759028041ca3c2db56b34a2c995f4d
jProgr <8537691@gmail.com>
2019-11-15 20:48:10 -0600

    Delete and add view from scratch

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -2,16 +2,8 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    @IBOutlet var labelResult: UILabel!
-    @IBOutlet var textMessage: UITextField!
-
     override func viewDidLoad()
     {
         super.viewDidLoad()
-    }
-    
-    @IBAction func changeButton(_ sender: UIButton)
-    {
-        labelResult.text = textMessage.text?.uppercased()
     }
 }
