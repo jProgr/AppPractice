62b5b9cf7a3eb2368bcce97c9cccea342dd3dd01
jProgr <8537691@gmail.com>
2019-11-18 12:53:09 -0600

    UI actions and custom attributes via code

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -2,8 +2,19 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet var mainLabel: UILabel!
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
+    }
+    
+    @IBAction func changeLabel(_ sender: UIButton)
+    {
+        mainLabel.text = "Text created by Swift \ncode using Xcode"
+        mainLabel.numberOfLines = 2
+        mainLabel.font = UIFont(name: "Courier", size: 14)
+        mainLabel.backgroundColor = UIColor.yellow
+        mainLabel.isEnabled = false
     }
 }
