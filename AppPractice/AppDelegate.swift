09f263e46f7c7681eba4c077682afbd5bfba0130
jProgr <8537691@gmail.com>
2019-11-18 10:34:39 -0600

    Interface objects and attributes

--- AppPractice/AppDelegate.swift
+++ AppPractice/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  AppPractice
-//
-//  Created by Cristóbal on 09/11/19.
-//  Copyright © 2019 jProgr. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
--- AppPractice/SceneDelegate.swift
+++ AppPractice/SceneDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  SceneDelegate.swift
-//  AppPractice
-//
-//  Created by Cristóbal on 09/11/19.
-//  Copyright © 2019 jProgr. All rights reserved.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
