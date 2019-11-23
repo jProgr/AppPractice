feb156c9a7c94a3e8c9ce97bd696e6c93bf28736
jProgr <8537691@gmail.com>
2019-11-23 12:47:42 -0600

    Add stepper
    Updated view to work with a stepper, button and label.

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -2,56 +2,20 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    @IBOutlet var textField: UITextField!
-    @IBOutlet var otherTextField: UITextField!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var stepper: UIStepper!
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(keyboardWillShow),
-            name: UIResponder.keyboardWillShowNotification,
-            object: nil)
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(keyboardWillHide),
-            name: UIResponder.keyboardWillHideNotification,
-            object: nil)
-
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        view.addGestureRecognizer(tap)
-        
-        textField.placeholder = "Email address here"
-        textField.textColor = UIColor.red
-        textField.font = UIFont(name: "Courier", size: 16)
-        textField.clearButtonMode = .whileEditing
     }
     
-    @objc func dismissKeyboard()
+    @IBAction func stepperChanged(_ sender: UIStepper)
     {
-        view.endEditing(true)
-    }
-
-    @objc func keyboardWillShow(notification: NSNotification)
-    {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
-        {
-            if self.view.frame.origin.y == 0 { self.view.frame.origin.y = -keyboardSize.height }
-        }
-    }
-
-    @objc func keyboardWillHide(notification: NSNotification)
-    {
-        if ((notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue) != nil
-        {
-            if self.view.frame.origin.y != 0 { self.view.frame.origin.y = 0 }
-        }
+        label.text = "\(stepper.value)"
     }
     
-    @IBAction func displayPassword(_ sender: UIButton)
+    @IBAction func changeStepper(_ sender: UIButton)
     {
-        textField.text = otherTextField.text?.uppercased()
     }
 }
