2202d9a4fa93c101767f23890648c5bf7eb8a8c0
jProgr <8537691@gmail.com>
2019-11-23 17:26:32 -0600

    Enable touch on image view

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -2,12 +2,19 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    
+    @IBOutlet var imageView: UIImageView!
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
+
+        imageView.isUserInteractionEnabled = true
     }
     
-   
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        let touch = touches.first
+        if touch?.view == imageView { print("Touched") }
+        else { print("Nothing") }
+    }
 }
