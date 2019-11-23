a2e14e02bd97692925929736002931632eabd587
jProgr <8537691@gmail.com>
2019-11-23 17:19:30 -0600

    Add image view and image

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -2,26 +2,12 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    @IBOutlet var label: UILabel!
-    @IBOutlet var stepper: UIStepper!
+    
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
     }
     
-    @IBAction func stepperChanged(_ sender: UIStepper)
-    {
-        label.text = "\(stepper.value)"
-    }
-    
-    @IBAction func changeStepper(_ sender: UIButton)
-    {
-        stepper.minimumValue = -10
-        stepper.maximumValue = -5
-        stepper.stepValue = 0.5
-        stepper.isContinuous = true
-        stepper.autorepeat = true
-        stepper.wraps = true
-    }
+   
 }
