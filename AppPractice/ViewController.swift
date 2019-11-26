6587846062f8831c3c09fe2ba29b80d619f36eff
jProgr <8537691@gmail.com>
2019-11-25 20:31:00 -0600

    Interactive label

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -2,22 +2,13 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    @IBOutlet var label: UILabel!
-    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
-        label.frame.size.width = 120
     }
     
-    @IBAction func buttonTapped(_ sender: Any)
+    @IBAction func tapped(_ sender: UITapGestureRecognizer)
     {
-        switch (sender as AnyObject).tag
-        {
-            case 1: label.text = "Button 1"
-            case 2: label.text = "Button 2"
-            default: label.text = "Default"
-        }
+        print("Tap detected")
     }
 }
