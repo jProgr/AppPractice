7cb62080fd38ab87fbdc5bc4a1a32ece1ebbbd4b
jProgr <8537691@gmail.com>
2019-11-29 22:23:16 -0600

    Interactive label via code

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -2,9 +2,20 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet var labelCode: UILabel!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        labelCode.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        labelCode.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTap()
+    {
+        print("Tap on second label detected")
     }
     
     @IBAction func tapped(_ sender: UITapGestureRecognizer)
