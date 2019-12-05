f0142bb9f58f5305cfea5f9964bb7a4338c2ec35
jProgr <8537691@gmail.com>
2019-12-04 20:58:26 -0600

    Multiple option alert handling

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -2,18 +2,31 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        label.numberOfLines = 0
     }
-
+    
     @IBAction func buttonTapped(_ sender: UIButton)
     {
-        let alert = UIAlertController(title: "Warning", message: "Zombies are loose!", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Warning", message: "OwO", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK",
-                                     style: .default,
-                                     handler: { action -> Void in})
+            style: .default,
+            handler: { action -> Void in self.label.text = "OK" })
+        let cancelAction = UIAlertAction(title: "Cancel",
+            style: .cancel,
+            handler: { action -> Void in self.label.text = "Cancel" })
+        let destroyAction = UIAlertAction(title: "Destroy",
+            style: .destructive,
+            handler: { action -> Void in self.label.text = "Destroy" })
         alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        alert.addAction(destroyAction)
+        
         self.present(alert, animated: true, completion: nil)
     }
 }
