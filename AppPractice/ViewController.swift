5e9a952d96989a7d4474defbac0d2a37e03f075d
jProgr <8537691@gmail.com>
2019-12-14 14:30:58 -0600

    Table view visually

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -1,14 +1,24 @@
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
-    @IBOutlet var petTable: UITableView!
-    let source = SourceDelegate()
-    
+    let petArray = ["cat", "dog", "parakeet", "parrot", "canary", "finch", "tropical fish", "goldfish", "sea horses", "hamster", "gerbil", "rabbit", "turtle", "snake", "lizard", "hermit crab"]
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return petArray.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID")
+        cell?.textLabel?.text = petArray[indexPath.row]
         
-        petTable.dataSource = source
+        return cell!
     }
 }
