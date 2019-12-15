e142198d1ce378fa441bd592bd7b4f909c74e443
jProgr <8537691@gmail.com>
2019-12-15 10:13:44 -0600

    Table view with sections

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -2,37 +2,51 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
-    let petArray = ["cat", "dog", "parakeet", "parrot", "canary", "finch", "tropical fish", "goldfish", "sea horses", "hamster", "gerbil", "rabbit", "turtle", "snake", "lizard", "hermit crab"]
+    let petArray = [["Mammal", "cat", "dog", "hamster", "gerbil", "rabbit"], ["Bird", "parakeet", "parrot", "canary", "finch"], ["Fish", "tropical fish", "goldfish", "sea horses"], ["Reptile", "turtle", "snake", "lizard"]]
+    let cellID = "cellID"
+
+    @IBOutlet var petTable: UITableView!
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
+
+        petTable.dataSource = self
+        petTable.delegate = self
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?
+    {
+        return petArray[section][0]
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int
     {
         return petArray.count
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return petArray[section].count - 1
+    }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID")
-        cell?.textLabel?.text = petArray[indexPath.row]
-        
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        if (cell == nil )
+        {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: cellID)
+        }
+        cell?.textLabel?.text = petArray[indexPath.section][indexPath.row + 1]
         return cell!
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        print(indexPath)
-        let selectedItem = petArray[indexPath.row]
+        let selectedItem = petArray[indexPath.section][indexPath.row + 1]
         let alert = UIAlertController(title: "Your Choice", message: "\(selectedItem)", preferredStyle: .alert)
-        let okAction = UIAlertAction(
-            title: "OK",
-            style: .default,
-            handler: { action -> Void in }
-        )
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in })
         alert.addAction(okAction)
-        self.present(alert, animated: true , completion: nil)
+        self.present(alert, animated: true, completion: nil)
     }
 }
