bdc774d6c75eb4e681dc21827da3f10193cd302c
jProgr <8537691@gmail.com>
2019-12-16 21:06:37 -0600

    Table view controller

---
+++ AppPractice/TableViewController.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+class TableViewController: UITableViewController
+{
+    let petArray = ["cat", "dog", "parakeet", "parrot", "canary", "finch", "tropical fish", "goldfish", "sea horses", "hamster", "gerbil", "rabbit", "turtle", "snake", "lizard", "hermit crab"]
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return petArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID")
+        cell?.textLabel?.text = petArray[indexPath.row]
+
+        return cell!
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        let selectedItem = petArray[indexPath.row]
+        let alert = UIAlertController(title: "Your Choice", message: "\(selectedItem)", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in })
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+}
