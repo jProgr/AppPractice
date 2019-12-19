901cecb1d40591846abfc0846c38a2ead1019aac
jProgr <8537691@gmail.com>
2019-12-19 17:46:48 -0600

    Table view with custom cell

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -2,19 +2,18 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
-    @IBOutlet var tableData: UITableView!
-
     let mainArray = ["Shuttle bus", "Hierarchy", "Exchange", "Padlock"]
     let detailArray = ["6am 10pm", "Acme corporation", "Ideas worth sharing", "Access denied"]
     let imageArray = ["1.png", "2.png", "3.png", "4.png"]
-    let cellID = "cellID"
+
+    @IBOutlet var tableData: UITableView!
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
-        tableData.delegate = self
+        
         tableData.dataSource = self
+        tableData.delegate = self
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
@@ -24,16 +23,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
-        if (cell == nil)
-        {
-            cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: cellID)
-        }
-        cell?.textLabel?.text = mainArray[indexPath.row]
-        cell?.detailTextLabel?.text = detailArray[indexPath.row]
-        cell?.imageView?.image = UIImage(named: imageArray[indexPath.row])
+        let cell: TableViewCell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! TableViewCell
+        cell.mainText?.text = self.mainArray[indexPath.row]
+        cell.detailText?.text = self.detailArray[indexPath.row]
+        cell.cellImage?.image = UIImage(named: self.imageArray[indexPath.row])
 
-        return cell!
+        return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
