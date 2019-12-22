7985ad35b3e3a1cf13434064f80af0fd61968a2e
jProgr <8537691@gmail.com>
2019-12-22 10:14:23 -0600

    Collection view

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -1,71 +1,30 @@
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate
 {
-    var petArray = ["cat", "dog", "parakeet", "parrot", "canary", "finch", "tropical fish", "goldfish", "sea horses", "hamster", "gerbil", "rabbit", "turtle", "snake", "lizard", "hermit crab"]
-    let cellID = "cellID"
-
-    @IBOutlet var tableData: UITableView!
+    @IBOutlet var collectionView: UICollectionView!
+    var cellColor = true
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
-        tableData.dataSource = self
-        tableData.delegate = self
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
     {
-        return petArray.count
+        return 100
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
     {
-        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
-        if (cell == nil)
-        {
-            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: cellID)
-        }
-        cell?.textLabel?.text = petArray[indexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath)
+        if cellColor { cell.backgroundColor = UIColor.yellow }
+        else { cell.backgroundColor = UIColor.green }
+        cellColor = !cellColor
 
-        return cell!
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-    {
-        let selectedItem = petArray[indexPath.row]
-        let alert = UIAlertController(title: "Your Choice", message: "\(selectedItem)", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in })
-        alert.addAction(okAction)
-        self.present(alert, animated: true, completion: nil)
-    }
-
-    func displayAlert(location: Int)
-    {
-        let alert = UIAlertController(title: "Add", message: "New Pet", preferredStyle: .alert)
-        alert.addTextField(configurationHandler: { (textField) in textField.placeholder = "Pet type here" })
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in
-            let savedText = alert.textFields![0] as UITextField
-            self.petArray.insert(savedText.text ?? "default", at: location)
-            self.tableData.reloadData()
-        })
-        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: { action -> Void in })
-        alert.addAction(okAction)
-        alert.addAction(cancelAction)
-        self.present(alert, animated: true, completion: nil)
-    }
-
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?
-    {
-        let addAction = UIContextualAction(style: .normal, title: "Add") { (action: UIContextualAction, sourceView: UIView, completion) in
-            self.displayAlert(location: indexPath.row)
-        }
-        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action: UIContextualAction, sourceView: UIView, completion) in
-            self.petArray.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
-        }
-
-        return UISwipeActionsConfiguration(actions: [addAction, deleteAction])
+        return cell
     }
 }
