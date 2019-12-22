314e1b012d2cd6d97bb6e0a4f5fec5e030fb9eb2
jProgr <8537691@gmail.com>
2019-12-21 18:09:33 -0600

    Table view with swipe actions

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -2,41 +2,70 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
-    let mainArray = ["Shuttle bus", "Hierarchy", "Exchange", "Padlock"]
-    let detailArray = ["6am 10pm", "Acme corporation", "Ideas worth sharing", "Access denied"]
-    let imageArray = ["1.png", "2.png", "3.png", "4.png"]
+    var petArray = ["cat", "dog", "parakeet", "parrot", "canary", "finch", "tropical fish", "goldfish", "sea horses", "hamster", "gerbil", "rabbit", "turtle", "snake", "lizard", "hermit crab"]
+    let cellID = "cellID"
 
     @IBOutlet var tableData: UITableView!
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
+
         tableData.dataSource = self
         tableData.delegate = self
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return mainArray.count
+        return petArray.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let cell: TableViewCell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! TableViewCell
-        cell.mainText?.text = self.mainArray[indexPath.row]
-        cell.detailText?.text = self.detailArray[indexPath.row]
-        cell.cellImage?.image = UIImage(named: self.imageArray[indexPath.row])
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        if (cell == nil)
+        {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: cellID)
+        }
+        cell?.textLabel?.text = petArray[indexPath.row]
 
-        return cell
+        return cell!
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        let selectedItem = mainArray[indexPath.row]
+        let selectedItem = petArray[indexPath.row]
         let alert = UIAlertController(title: "Your Choice", message: "\(selectedItem)", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in })
         alert.addAction(okAction)
         self.present(alert, animated: true, completion: nil)
+    }
+
+    func displayAlert(location: Int)
+    {
+        let alert = UIAlertController(title: "Add", message: "New Pet", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: { (textField) in textField.placeholder = "Pet type here" })
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in
+            let savedText = alert.textFields![0] as UITextField
+            self.petArray.insert(savedText.text ?? "default", at: location)
+            self.tableData.reloadData()
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: { action -> Void in })
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?
+    {
+        let addAction = UIContextualAction(style: .normal, title: "Add") { (action: UIContextualAction, sourceView: UIView, completion) in
+            self.displayAlert(location: indexPath.row)
+        }
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action: UIContextualAction, sourceView: UIView, completion) in
+            self.petArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
+
+        return UISwipeActionsConfiguration(actions: [addAction, deleteAction])
     }
 }
