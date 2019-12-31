2bd1c0b881408ba0c16b4e61514f7b1cfb2286ef
jProgr <8537691@gmail.com>
2019-12-31 10:22:48 -0600

    Navigation controller with multiple bar buttons

--- AppPractice/SecondViewController.swift
+++ AppPractice/SecondViewController.swift
@@ -6,4 +6,13 @@ class SecondViewController: UIViewController
     {
         super.viewDidLoad()
     }
+
+    @IBAction func doneTapped(_ sender: Any)
+    {
+        let alert = UIAlertController(title: "Message", message: "Done button tapped", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in })
+        alert.addAction(okAction)
+
+        self.present(alert, animated: true, completion: nil)
+    }
 }
