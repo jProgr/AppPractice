72691277d3f0424afe419ff14831343fdd652373
jProgr <8537691@gmail.com>
2019-12-31 10:13:31 -0600

    Navigation controller with button on bar

--- AppPractice/SecondViewController.swift
+++ AppPractice/SecondViewController.swift
@@ -6,15 +6,4 @@ class SecondViewController: UIViewController
     {
         super.viewDidLoad()
     }
-
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
-    {
-        performSegue(withIdentifier: "secondLink", sender: self)
-    }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-    {
-        let nextVC = segue.destination as! ThirdViewController
-        nextVC.navigationItem.title = "Third View Controller"
-    }
 }
--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController
         performSegue(withIdentifier: "firstLink", sender: self)
     }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    @IBAction func buttonTapped(_ sender: UIBarButtonItem)
     {
-        let nextVC = segue.destination as! SecondViewController
-        nextVC.navigationItem.title = "Second View Controller"
+        let alert = UIAlertController(title: "Message", message: "Bar button tapped", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in })
+        alert.addAction(okAction)
+
+        self.present(alert, animated: true, completion: nil)
     }
 }
