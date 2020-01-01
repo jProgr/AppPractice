ce437034718e1539ad4e05230c36ab6a07c6caa2
jProgr <8537691@gmail.com>
2020-01-01 16:52:49 -0600

    Simple tab bar controller

--- AppPractice/ViewController.swift
+++ AppPractice/ViewController.swift
@@ -6,18 +6,4 @@ class ViewController: UIViewController
     {
         super.viewDidLoad()
     }
-
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
-    {
-        performSegue(withIdentifier: "firstLink", sender: self)
-    }
-
-    @IBAction func buttonTapped(_ sender: UIBarButtonItem)
-    {
-        let alert = UIAlertController(title: "Message", message: "Bar button tapped", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in })
-        alert.addAction(okAction)
-
-        self.present(alert, animated: true, completion: nil)
-    }
 }
