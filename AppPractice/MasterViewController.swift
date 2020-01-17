3c8eaaf09eb514f574030eb24d8f4a92030083a0
jProgr <8537691@gmail.com>
2020-01-16 20:58:14 -0600

    Add basic functionality
    Based on: UISplitViewController Tutorial
    https://www.raywenderlich.com/4613809-uisplitviewcontroller-tutorial-getting-started

---
+++ AppPractice/MasterViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class MasterViewController: UITableViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        return cell
+    }
+}
