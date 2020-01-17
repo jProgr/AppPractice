3d1da631097544b99bb3b90bbdb4ce78cfc2666b
jProgr <8537691@gmail.com>
2020-01-16 21:07:57 -0600

    Add Monster model
    Based on: UISplitViewController Tutorial
    https://www.raywenderlich.com/4613809-uisplitviewcontroller-tutorial-getting-started

---
+++ AppPractice/Monster.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class Monster
+{
+    let name: String
+    let description: String
+    let iconName: String
+    let weapon: Weapon
+
+    init(name: String, description: String, iconName: String, weapon: Weapon)
+    {
+        self.name = name
+        self.description = description
+        self.iconName = iconName
+        self.weapon = weapon
+    }
+
+    var icon: UIImage?
+    {
+        return UIImage(named: iconName)
+    }
+}
+
+enum Weapon
+{
+    case blowgun, ninjaStar, fire, sword, smoke
+
+    var image: UIImage
+    {
+        switch self
+        {
+            case .blowgun: return UIImage(named: "blowgun.png")!
+            case .fire: return UIImage(named: "fire.png")!
+            case .ninjaStar: return UIImage(named: "ninjastar.png")!
+            case .smoke: return UIImage(named: "smoke.png")!
+            case .sword: return UIImage(named: "sword.png")!
+        }
+    }
+}
