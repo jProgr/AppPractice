31ea2811131f1152e63381f841ceb5cf828f011c
jProgr <8537691@gmail.com>
2020-01-17 22:25:24 -0600

    Add models to list
    Based on: UISplitViewController Tutorial
    https://www.raywenderlich.com/4613809-uisplitviewcontroller-tutorial-getting-started

--- AppPractice/MasterViewController.swift
+++ AppPractice/MasterViewController.swift
@@ -2,6 +2,16 @@ import UIKit
 
 class MasterViewController: UITableViewController
 {
+    let monsters =
+    [
+        Monster(name: "Cat-Bot", description: "MEE-OW", iconName: "meetcatbot", weapon: .sword),
+        Monster(name: "Dog-Bot", description: "BOW-WOW", iconName: "meetdogbot", weapon: .blowgun),
+        Monster(name: "Explode-Bot", description: "BOOM!", iconName: "meetexplodebot", weapon: .smoke),
+        Monster(name: "Fire-Bot", description: "Will Make You Steamed", iconName: "meetfirebot", weapon: .ninjaStar),
+        Monster(name: "Ice-Bot", description: "Has A Chilling Effect", iconName: "meeticebot", weapon: .fire),
+        Monster(name: "Mini-Tomato-Bot", description: "Extremely Handsome", iconName: "meetminitomatobot", weapon: .ninjaStar)
+    ]
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -14,12 +24,14 @@ class MasterViewController: UITableViewController
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 10
+        return monsters.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let monster = monsters[indexPath.row]
+        cell.textLabel?.text = monster.name
 
         return cell
     }
